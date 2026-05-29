ad38ff96dd9fd124de77b93f2294aa55752845a7
AlonsoX1809 <a01646106@tec.mx>
2026-05-29 15:59:46 -0600

    Update top_tb.v

--- Single_cycle/top_tb.v
+++ Single_cycle/top_tb.v
@@ -1,35 +1,24 @@
 // Miguel Alonso De La Rosa Zamora A01646106
- 
 module top_tb();
 
-	reg [31:0] A, B;
-	reg [2:0] Control;
-	wire [31:0] Result;
-	wire Zero;
-	
-	ALU dut(.A(A), .B(B), .Control(Control), .Result(Result), .Zero(Zero));
-	initial
-		begin
-		$display("Simulacion iniciada");
-		repeat(80)
-		begin 
-			A = $random %100;
-			B = $random %100;
-			Control = $random %6;
-			#10;
-		end
-		$display("Simulacion finalizada");
-		$stop;
-		$finish;
-		end
-	
-		initial
-			begin
-				$monitor("A = %b, B = %b, Control = %b, Result = %b, Zero = %b", A, B, Control, Result, Zero);
-			end
-		initial
-			begin
-				$dumpfile("ALU_tb.vcd");
-				$dumpvars(0, ALU_tb);
-			end
-endmodule
+reg clk, rst;
+top dut ( .clk(clk), .rst(rst));
+always #5 clk = ~clk; 
+initial begin
+    clk = 0;
+    rst  = 1;
+    #10;
+    rst  = 0;
+    #100;
+    $finish;
+end
+
+initial begin
+    $dumpfile("single_cycle.vcd");
+    $dumpvars(0, top_tb);
+end
+
+initial begin
+    $display("clk | rst | PC | Instr | ALUResult | WD3 | RegWrite | MemWrite | PCSrc");
+    $monitor("%b | %b | %h | %h | %h | %h | %b | %b | %b", clk, rst, dut.PC, dut.Instr, dut.ALUResult, dut.WD3, dut.RegWrite, dut.MemWrite, dut.PCSrc);
+end
